5953b7b90420d5684b36b3e4fd9d71b858bccfb4
Jon Whittlestone <dev@howapped.com>
2015-03-09 11:05:25 +0000

    initial commit

---
+++ src/Jonwhittlestone/Larability/Facades/Larability.php
@@ -0,0 +1,14 @@
+<?php namespace Jonwhittlestone\Larability\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Larability extends Facade {
+
+  /**
+   * Get the registered name of the component.
+   *
+   * @return string
+   */
+  protected static function getFacadeAccessor() { return 'larability'; }
+
+}
